a6c7c96445745ac92422b1dfa8abb04dfb5dc381
Elisa BOURG <elisabourg@macbook-air-de-elisa-1.home>
2020-04-22 12:04:51 +0200

    tentative 120000

---
+++ src/Controller/UserController.php
@@ -0,0 +1,47 @@
+<?php
+
+
+namespace App\Controller;
+
+use App\Model\UserManager;
+use App\Services\UserValidator;
+
+class UserController extends AbstractController
+{
+    public function add()
+    {
+
+        $errors = [];
+        $user = [];
+        $noError = "";
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            // Prepare user array from POST data
+            $user = [
+                'pseudo'=> $_POST['pseudo'],
+                'email'=> $_POST['email'],
+                'password'=> $_POST['password']
+
+            ];
+
+            // Check if user is valid
+            $validation = new UserValidator($user);
+            $validation->validateForm();
+           // $noError = $validation->noError(); ne marche pas
+            $user['pseudo'] = $validation->clean($user['pseudo']);
+
+            // If no error, insert user in DB
+            $user['password'] = password_hash($user['password'], PASSWORD_BCRYPT);
+            $userManager = new UserManager;
+            $userManager->insert($user);
+            //$noError = $validation->noError();
+        }
+        //  header('Location:/home/index/' ); redirection
+
+        return $this->twig->render('User/add.html.twig', [
+            'errors' => $errors,
+            'user' => $user,
+            'no_error'=> $noError,
+
+        ]); //page twig
+    }
+}
