41e265c89d936393595aff02a776bf7b97b23796
Amelie <a.grn33@gmail.com>
2020-04-28 19:11:29 +0200

    homepage arrangement

--- public/assets/js/iframeNotHidden.js
+++ public/assets/js/iframeNotHidden.js
@@ -1,10 +1,10 @@
 let iframes = document.getElementsByTagName('iframe');
 let selectors = document.getElementsByClassName('song');
 
-
 for (let i = 0; i< selectors.length; i++){
   selectors[i].addEventListener('click', (e)=>{
-      iframes[i].classList.toggle('hidden');
+      iframes[i].classList.remove('hidden');
+      iframes[i].classList.add('display');
   })
 }
 
