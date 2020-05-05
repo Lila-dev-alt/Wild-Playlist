76fd8dfbb4c833dd7e90127352b1866972fd673f
fredstp <freal1802@gmail.com>
2020-05-05 12:03:32 +0200

    form ok

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -3,7 +3,6 @@
 
 namespace App\Controller;
 
-
 use App\Model\QuestionManager;
 
 class QuestionController extends AbstractController
@@ -11,19 +10,22 @@ class QuestionController extends AbstractController
 
     public function add()
     {
-
+        $error=[];
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            $questionManager = new QuestionManager();
-            $question = [
-                'content' => $_POST['content'],
-            ];
-            $questionManager->insert($question);
-            header('Location:/question/add/');
-
-            exit;
+            if (empty($_POST['content'])) {
+                $error['erreur']= 'la question est vide';
+            } else {
+                $questionManager = new QuestionManager();
+                $question = [
+                    'content' => $_POST['content'],
+                ];
+                $questionManager->insert($question);
+                header('Location:/question/add/');
+                exit();
+            }
         }
-
-        return $this->twig->render('question/add.html.twig');
+        return $this->twig->render('question/add.html.twig', [
+            'error' => $error,
+        ]);
     }
-
 }
