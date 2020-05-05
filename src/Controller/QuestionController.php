0513fb1b1a4cac030dc18456518d4e711889115b
Ludovic Dureau <ludodureau@gmail.com>
2020-05-05 15:32:38 +0200

    admin edit/delete questions

---
+++ src/Controller/QuestionController.php
@@ -0,0 +1,64 @@
+<?php
+
+
+namespace App\Controller;
+
+use App\Model\QuestionManager;
+
+/**
+ * Class QuestionController
+ *
+ */
+class QuestionController extends AbstractController
+{
+    /**
+     * @return string
+     * @throws \Twig\Error\LoaderError
+     * @throws \Twig\Error\RuntimeError
+     * @throws \Twig\Error\SyntaxError
+     */
+    public function show()
+    {
+        $questionManager = new QuestionManager();
+        $questions = $questionManager->selectAll();
+
+        return $this->twig->render('Question/all.html.twig', ['questions' => $questions]);
+    }
+
+    /**
+     * Handle id deletion
+     *
+     * @param int $id
+     */
+    public function delete(int $id)
+    {
+        $questionManager = new QuestionManager();
+        $questionManager->delete($id);
+        header('Location:/question/show');
+
+        exit();
+    }
+
+    /**
+    * Display item edition page specified by $id
+    *
+    * @param int $id
+    * @return string
+     */
+    public function edit(int $id): string
+    {
+        $questionManager = new QuestionManager();
+        $question = $questionManager->selectOneById($id);
+
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $questionManager->update($_POST);
+            // TODO: faut-il rediriger vers show ou laisser sur edit ?
+            header('Location:/question/edit/' . $_POST['id']);
+        }
+
+        return $this->twig->render('Question/edit.html.twig', ['question' => $question]);
+    }
+
+
+
+}
---
+++ src/Model/QuestionManager.php
@@ -0,0 +1,68 @@
+<?php
+
+
+namespace App\Model;
+
+
+/**
+ *
+ */
+class QuestionManager extends AbstractManager
+{
+    /**
+     *
+     */
+    const TABLE = 'question';
+
+    /**
+     *  Initializes this class.
+     */
+    public function __construct()
+    {
+        parent::__construct(self::TABLE);
+    }
+
+
+    /**
+     * @param array $question
+     * @return int
+     */
+    public function insert(array $question): int
+    {
+        // prepared request
+        $statement = $this->pdo->prepare("INSERT INTO " . self::TABLE . " (`content`) VALUES (:content)");
+        $statement->bindValue('content', $question['content'], \PDO::PARAM_STR);
+
+        if ($statement->execute()) {
+            return (int)$this->pdo->lastInsertId();
+        }
+    }
+
+
+    /**
+     * @param int $id
+     */
+    public function delete(int $id): void
+    {
+        // prepared request
+        $statement = $this->pdo->prepare("DELETE FROM " . self::TABLE . " WHERE id=:id");
+        $statement->bindValue('id', $id, \PDO::PARAM_INT);
+        $statement->execute();
+    }
+
+
+    /**
+     * @param array $question
+     * @return bool
+     */
+    public function update(array $question):bool
+    {
+
+        // prepared request
+        $statement = $this->pdo->prepare("UPDATE " . self::TABLE . " SET `content` = :content WHERE id=:id");
+        $statement->bindValue(':id', $question['id'], \PDO::PARAM_INT);
+        $statement->bindValue(':content', $question['content'], \PDO::PARAM_STR);
+
+        return $statement->execute();
+    }
+}
